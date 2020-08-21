a32dc5f09be1050daa42d7b070aa5752c910a891
yu.zuomin <yu.zuomin@ebiz-digits.com>
2020-08-21 11:08:53 +0800

    BGX-0001 GOF 完成设计模式

---
+++ base/source/spring-faker/src/main/java/com/baogex/architect/base/source/spring5/gof/observer/bgeradvice/Question.java
@@ -0,0 +1,19 @@
+package com.baogex.architect.base.source.spring5.gof.observer.bgeradvice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * <p>
+ * 问题xueshengg
+ * </p>
+ *
+ * @author : zuomin.yu
+ * @date : 2020-08-21
+ */
+@Data
+@AllArgsConstructor
+public class Question {
+    private String name;
+    private String content;
+}
---
+++ base/source/spring-faker/src/main/java/com/baogex/mvcframework/annotation/BGService.java
@@ -0,0 +1,13 @@
+package com.baogex.mvcframework.annotation;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author : zuomin.yu
+ * @date : 2020-08-21
+ */
+public @interface BGService {
+    
+}
